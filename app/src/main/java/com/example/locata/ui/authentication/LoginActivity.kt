756abc0a2eb19d5dd082069637c828a206610ca7
Aviithapa <abhishekthapa115@gmail.com>
2021-06-18 20:50:14 +0545

    Register Activity

---
+++ app/src/main/java/com/example/locata/ui/authentication/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.locata.ui.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.locata.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/locata/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.locata.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.locata.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
